be110f0d3fd540d08d67bc22e2c6edf97599bc03
nehakadam1025 <neha.kadam@gmail.com>
2026-01-13 23:17:01 -0600

    User module and Excel data

--- src/test/java/stepdefinitions/UsermoduleSteps.java
+++ src/test/java/stepdefinitions/UsermoduleSteps.java
@@ -1,25 +1,6 @@
 package stepdefinitions;
 
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-//public class UsermoduleSteps {
-//	
-//	@Given("Admin sets Bearer token")
-//	public void admin_sets_bearer_token() {
-//	    
-//	}
-//
-//	@Given("Admin creates POST Request for the LMS API endpoint")
-//	public void admin_creates_post_request_for_the_lms_api_endpoint() {
-//	   
-//	}
-//
-//	@When("Admin sends HTTPS Request with mandatory and additional fields")
-//	public void admin_sends_https_request_with_mandatory_and_additional_fields() {
-//=======
 import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
@@ -29,6 +10,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import pojo.CreatedUserData;
 import pojo.LoginTempData;
 import utils.ApiResources;
 import utils.TestDataBuild;
@@ -39,49 +21,20 @@ public class UsermoduleSteps extends utility {
 	private RequestSpecification req;
     private Response response;
 
+
     TestDataBuild data1 = new TestDataBuild();
 
 	@Given("Admin creates POST Request for the LMS API endpoint")
 	public void admin_creates_post_request_for_the_lms_api_endpoint() throws IOException {
 		  
-//		String userPayload = "{\r\n"
-//				+ "  \"userComments\": \"APHackathons\",\r\n"
-//				+ "  \"userEduPg\": \"Msc\",\r\n"
-//				+ "  \"userEduUg\": \"Bsc\",\r\n"
-//				+ "  \"userFirstName\": \"Rinarrc\",\r\n"
-//				+ "  \"userLastName\": \"Rinarrc\",\r\n"
-//				+ "  \"userLinkedinUrl\": \"https://www.linkedin.com/Rinarrc\",\r\n"
-//				+ "  \"userLocation\": \"USA\",\r\n"
-//				+ "  \"userMiddleName\": \"Rajan\",\r\n"
-//				+ "  \"userPhoneNumber\": \"+91 9934567890\",\r\n"
-//				+ "  \"userRoleMaps\": [\r\n"
-//				+ "    {\r\n"
-//				+ "      \"roleId\": \"R01\",\r\n"
-//				+ "      \"userRoleStatus\": \"Active\"\r\n"
-//				+ "    }\r\n"
-//				+ "  ],\r\n"
-//				+ "  \"userTimeZone\": \"EST\",\r\n"
-//				+ "  \"userVisaStatus\": \"H1B\",\r\n"
-//				+ "  \"userLogin\": {\r\n"
-//				+ "    \"userLoginEmail\": \"Rinarrc@gmail.com\",\r\n"
-//				+ "    \"loginStatus\": \"Active\",\r\n"
-//				+ "    \"status\": \"Active\"\r\n"
-//				+ "  }\r\n"
-//				+ "}";
 
-//		  String token = LoginTempData.getToken(); // token from login feature
-//
-//	        req = given()
-//	                .spec(requestspecification())
-//	                .header("Authorization", "Bearer " + token).body(data.createUserPayload());
 		String token = LoginTempData.getToken();
 
 	    req = given()
 	            .spec(requestspecification())
 	            .header("Authorization", "Bearer " + token)
 	            .body(data1.createUserPayload());
-
-//	   
+	   
 	}
 
 	@When("Admin calls {string} with {string} http request for user admin")
@@ -92,10 +45,22 @@ public class UsermoduleSteps extends utility {
 	        if (method.equalsIgnoreCase("POST")) {
 	            response = req.when().post(resourceAPI.getResorce());
 	        }
+	        
+		 /*if (method.equalsIgnoreCase("GET")) {
+	            response = req.when().get(resourceAPI.getResorce());
+	        }
+	        else if (method.equalsIgnoreCase("POST")) {
+	            response = req.when().post(resourceAPI.getResorce());
+	        }
+	            else if (method.equalsIgnoreCase("PUT")) {
+	            response = req.when().put(resourceAPI.getResorce());
+	        }*/
 	    
 	}
+	
 	@Then("Admin receives {int} Created Status with response body")
 	public void admin_receives_created_status_with_response_body(Integer expectedStatus) {
+		
 		int actualStatus = response.getStatusCode();
         System.out.println("Status Code: " + actualStatus);
         System.out.println("Response: " + response.asString());
@@ -103,4 +68,84 @@ public class UsermoduleSteps extends utility {
         assert actualStatus == expectedStatus :
                 "Expected " + expectedStatus + " but got " + actualStatus;
 	}
+
+
+
+
+	@Given("Admin creates GET Request for the user module")
+	public void admin_creates_get_request_for_the_user_module() throws IOException {
+		
+		String token = LoginTempData.getToken(); // token from login feature
+
+        req = given()
+                .spec(requestspecification())
+                .header("Authorization", "Bearer " + token);
+	
+	}
+	
+	@When("Admin calls {string} with {string} http request for User module")
+	public void admin_calls_with_http_request_for_user_module(String resource, String method) {
+		
+		ApiResources resourceAPI = ApiResources.valueOf(resource);
+
+        if (method.equalsIgnoreCase("GET")) {
+            response = req.when().get(resourceAPI.getResorce());
+        }
+		
+	}
+
+	@Then("Admin receives {int} Status for user module")
+	public void admin_receives_status_for_user_module(Integer expectedStatus) {
+	  
+		int actualStatus = response.getStatusCode();
+        System.out.println("Status Code: " + actualStatus);
+        System.out.println("Response: " + response.asString());
+
+        assert actualStatus == expectedStatus :
+                "Expected " + expectedStatus + " but got " + actualStatus;
+	}
+	
+	
+	@Given("Admin creates GET Request in the user module")
+	public void admin_creates_get_request_in_the_user_module() throws IOException {
+		
+		String token = LoginTempData.getToken(); // token from login feature
+
+        req = given()
+                .spec(requestspecification())
+                .header("Authorization", "Bearer " + token);
+	}
+
+	@When("Admin calls {string} with {string} http request in User module")
+	public void admin_calls_with_http_request_in_user_module(String resource, String method) {
+	    
+		ApiResources resourceAPI = ApiResources.valueOf(resource);
+
+        if (method.equalsIgnoreCase("GET")) {
+            response = req.when().get(resourceAPI.getResorce());
+        }
+	}
+
+	@Then("Admin receives {int} ok Status for user module")
+	public void admin_receives_ok_status_for_user_module(Integer expectedStatus) {
+	    
+		
+		int actualStatus = response.getStatusCode();
+        System.out.println("Status Code: " + actualStatus);
+        System.out.println("Response: " + response.asString());
+
+        assert actualStatus == expectedStatus :
+                "Expected " + expectedStatus + " but got " + actualStatus;
+        if (actualStatus == 201) {
+            String userId = response.jsonPath().getString("userId"); // Adjust based on your response
+            CreatedUserData.setUserId(userId);
+            System.out.println("Saved created user ID: " + userId);
+        }
+	}
+
+
+	
+	
+	
 }
+
--- src/test/resources/features/usermodule.feature
+++ src/test/resources/features/usermodule.feature
@@ -1,10 +1,18 @@
 Feature: User Module - Admin Management
-
-    
- @POST 
-  Scenario: Create admin with valid endpoint and request body
+  
+@POST 
+Scenario: Create admin with valid endpoint and request body
   Given Admin creates POST Request for the LMS API endpoint
   When Admin calls "createUser" with "POST" http request for user admin
   Then Admin receives 201 Created Status with response body
 
- 
+Scenario: Check if admin able to get all user module with valid endpoint 
+    Given Admin creates GET Request for the user module
+    When Admin calls "getallUsers" with "GET" http request for User module
+    Then Admin receives 200 Status for user module
+    
+Scenario: Check if admin able to get user by id with valid endpoint 
+    Given Admin creates GET Request in the user module
+    When Admin calls "getbyid" with "GET" http request in User module
+    Then Admin receives 200 ok Status for user module 
+
