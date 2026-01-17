84f9f33042fecd5877ac6375e647c19aebbff27e
Darshana4987 <darshanaadhav@gmail.com>
2026-01-17 09:58:47 -0500

    Update LoginRequest.java

--- src/main/java/pojo/LoginRequest.java
+++ src/main/java/pojo/LoginRequest.java
@@ -1,31 +1,3 @@
-//package pojo;
-//
-//public class LoginRequest {
-//    private String userLoginEmailId;
-//    private String password;
-//
-//    public LoginRequest(String userLoginEmailId, String password) {
-//        this.userLoginEmailId = userLoginEmailId;
-//        this.password = password;
-//    }
-//
-//    public String getUserLoginEmailId() {
-//        return userLoginEmailId;
-//    }
-//
-//    public void setUserLoginEmailId(String userLoginEmailId) {
-//        this.userLoginEmailId = userLoginEmailId;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
-//
 package pojo;
 
 public class LoginRequest {
